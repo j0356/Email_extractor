c99b1f2822af6b7b66bcfabdaddeef8b920791b0
j0356 <82762740+j0356@users.noreply.github.com>
2023-12-31 01:04:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import pwd
+
+word: str = "From: "
+Path: str = f'/Users/{pwd.getpwuid(os.getuid())[0]}/Desktop/Email_extractor/eml/'
+filelist = os.listdir(Path)
+
+for file in filelist:
+    if file.endswith(".eml"):
+         with open(Path + file, 'r') as f:
+            for i in f.readlines():
+                if word in i:
+                    result = i.replace("From: ", '').replace('>', '').replace('<', '').strip().split(' ')
+                    email = result[len(result) - 1]
+                    o = open('emails.csv', 'a')
+                    o.write(f'{email}\n')
